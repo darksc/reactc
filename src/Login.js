1e1910beabd1455e3aee2baf59855602c77e2012
darksc <123189717@qq.com>
2016-04-25 16:37:38 +0800

    update from validate

--- src/Login.js
+++ src/Login.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import React, { Component } from 'react';
 
 import Header from './Header';
@@ -7,8 +9,26 @@ export default class Login extends Component {
         return (
             <div>
                 <Header name={this.props.routes}/>
-                <h1>欢迎登录reactc</h1>
+                <div className="content">
+                    <div className="login">
+                        <div>
+                            <input type="text" placeholder="Email or Username" onChange={this.handleChange}/>
+                        </div>
+                        <div>
+                            <input type="password" placeholder="Password"/>
+                        </div>
+                        <div>
+                            <button>登录</button>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         );
+    }
+
+    handleChange(e) {
+        var value = e.target.value;
+        console.log(value, 'username');
     }
 }
--- src/Register.js
+++ src/Register.js
@@ -1,14 +1,66 @@
+'use strict';
+
 import React, { Component } from 'react';
 
 import Header from './Header';
 
 export default class Register extends Component {
+
+    constructor(props, context) {
+
+        super(props, context);
+
+        this.state = {
+            usernameVerify: true,
+            emailVerify: true
+        };
+    }
+
     render() {
+
         return (
             <div>
                 <Header name={this.props.routes}/>
-                <h1>欢迎注册reactc</h1>
+                <div className="content">
+                    <div className="register">
+                        <div>
+                            <input type="text" placeholder="Username" className={!this.state.usernameVerify ? 'error' : ''} onChange={this.usernameChange.bind(this)}/>
+                        </div>
+                        <div>
+                            <input type="email" placeholder="Email" className={!this.state.emailVerify ? 'error' : ''} onChange={this.emailChange.bind(this)}/>
+                        </div>
+                        <div>
+                            <input type="password" placeholder="Password"/>
+                        </div>
+                        <div>
+                            <input type="password" placeholder="Confirm Password"/>
+                        </div>
+                        <div>
+                            <button>登录</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
+    }
+
+    usernameChange(e) {
+        var value = e.target.value;
+        var reg =/^[a-zA-Z]{6,12}$/;
+        if (value.match(reg)) {
+            this.setState({usernameVerify: true});
+        }else{
+            this.setState({usernameVerify: false});
+        }
+    }
+
+    emailChange(e) {
+        var value = e.target.value;
+        var reg =/[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/;
+        if (value.match(reg)) {
+            this.setState({emailVerify: true});
+        }else{
+            this.setState({emailVerify: false});
+        }
     }
 }
